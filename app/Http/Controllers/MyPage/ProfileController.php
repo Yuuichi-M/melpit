f83884344fb0238ec8f4e817dfaad341408545f8
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2021-10-06 23:24:50 +0900

    プロフィール保存処理

--- app/Http/Controllers/MyPage/ProfileController.php
+++ app/Http/Controllers/MyPage/ProfileController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers\MyPage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Mypage\Profile\EditRequest;
 use Illuminate\Support\Facades\Auth;
 
+//プロフィール編集画面
 class ProfileController extends Controller
 {
     public function showProfileEditForm()
     {
         return view('mypage.profile_edit_form')
             ->with('user', Auth::user());
+    }
+
+    //プロフィール保存処理
+    public function editProfile(EditRequest $request)
+    {
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        return redirect()->back()
+            ->with('status', 'プロフィールを変更しました。');
     }
 }
